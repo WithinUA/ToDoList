8cc4e8000bdc3dcca14eeffe1cf5e4dd32e8673c
WithinUA <withinua@gmail.com>
2024-06-12 14:22:47 +0300

    added screenshots to assets

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+
   title = 'ToDoList';
+  firstName: string = `Valentin`;
+  lastName: string = `Verhovsky`;
+  age: number = 36;
+  selfInfo: string = "I tried coding in middle age and I liked it very much. I hope it's not too late to start now.";
+  
+
+  get FullName(): string {
+     return  `${this.firstName} ${this.lastName}`
+    }
 }
